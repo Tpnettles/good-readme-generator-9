bb47bf28dc7d230cf6b9ce7f16b264ed950dd15f
Tyree Nettles <nettles.code@gmail.com>
2021-07-11 18:30:01 -0500

    Final edits

--- README.md
+++ README.md
@@ -1,32 +1,48 @@
 
-# 
+  # Good readme generator
 
+  # Table of Contents
 
-## Description
+  - [Contributing](#Requirements_of_contributions)
 
+  - [Description](#Description)
 
+  - [Information](#Links)
 
-## Instructions
+  - [Installation](#Instructions)
 
+  - [License](#License)
 
-
-## Demonstrations/Examples
-
+  - [Usage](#Demonstrations/Examples)
 
   
-## Requirements of contributions
 
-
-
-## Testing
-
-
-
-## Links
-
-Here is the link to [my GitHub page]().
-
-## License 
-
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
+  ## Description
+  
+  This is an application that provides users with needed information to completely understand the application and the functionality of it.
+  
+  ## Instructions
+  
+  Invoke application by running node index in the terminal * fill out answers to prompted questions.
+  
+  ## Demonstrations/Examples
+  
+  We are doing that part here.
+    
+  ## Requirements_of_contributions
+  
+  None
+  
+  ## Testing
+  
+  None
+  
+  ## Links
+  
+  Here is my e-mail address [nettles.code@gmail.com].
+  Here is the link to my GitHub page [https://github.com/tpnettles].
+  
+  ## License 
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+   
--- index.js
+++ index.js
@@ -1,16 +1,91 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateMarkdown = require('./assets/generateMarkdown');
+// Code that constructs markdown page
+const generateMarkdown = (answers) =>
+  
+`
+  # ${answers.title}
+
+  # Table of Contents
+
+  - [Contributing](#Requirements_of_contributions)
+
+  - [Description](#Description)
+
+  - [Information](#Links)
+
+  - [Installation](#Instructions)
+
+  - [License](#License)
+
+  - [Usage](#Demonstrations/Examples)
+
+  
+
+  ## Description
+  
+  ${answers.description}
+  
+  ## Instructions
+  
+  ${answers.installation}
+  
+  ## Demonstrations/Examples
+  
+  ${answers.usage}
+    
+  ## Requirements_of_contributions
+  
+  ${answers.contributing}
+  
+  ## Testing
+  
+  ${answers.tests}
+  
+  ## Links
+  
+  Here is my e-mail address [${answers.email_information}].
+  Here is the link to my GitHub page [https://github.com/${answers.GitHub_information}].
+  
+  ## License 
+  
+  ${renderLicenseBadge(answers.license)}
+   `;
+
+
+
+const renderLicenseBadge = (license) => {
+
+    if(license === 'MIT'){
+    
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    }
+    else if(license === 'Mozilla'){
+      
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    }
+    else if(license === 'BSD_3'){
+    
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+    }
+    else if(license === 'Apache'){
+    
+      return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    }
+    else {
+      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    }
+    };
 
 
 inquirer
   .prompt([
-    /* Questions */
+    /* Questions ran in node */
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of the application that is associated with this README?',
+        message: 'What is the title of the application?',
       },
       {
         type: 'input',
@@ -43,14 +118,19 @@ inquirer
         message: 'What license will you be using?',
         choices: ['MIT','Mozilla','BSD_3', 'Apache'],
         default:'MIT'
+      },
+      {
+        type: 'input',
+        name: 'email_information',
+        message: 'Please enter the your email address.',
+        
       },      
       {
         type: 'input',
-        name: 'gitHub_information',
-        message: 'What is your GitHub username?',
+        name: 'GitHub_information',
+        message: 'Please enter your GitHub username.',
+        
       },
-
-
   ])
   .then((answers) => {
     console.log(answers)
@@ -60,3 +140,6 @@ inquirer
       err ? console.log(err) : console.log('README created!')
     );
   });
+
+  module.exports = generateMarkdown;
+
